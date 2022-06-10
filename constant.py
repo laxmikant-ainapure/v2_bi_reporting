9abf0f25595202ff2dbc19443d11d3a40b675bea
Laxmikant Ainapure <76990221+laxmikant-ainapure@users.noreply.github.com>
2022-06-10 16:44:52 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -4,7 +4,6 @@ internal_team_subject='V2 Production Repository and scan details : Auto generate
 developer_team_subject=''
 internal_team_list=''
 developer_list=''
-from_mail_password='123' *from_mail_password='123'
 from_mail_password='123'
 #DB configuration
 
