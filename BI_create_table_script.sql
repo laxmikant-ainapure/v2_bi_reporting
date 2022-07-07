0e6f24eca3264e2130cff5f0117870e9c083ff2b
Laxmikant Ainapure <76990221+laxmikant-ainapure@users.noreply.github.com>
2022-07-07 14:07:51 +0530

    Update BI_create_table_script.sql

--- BI_create_table_script.sql
+++ BI_create_table_script.sql
@@ -5,7 +5,6 @@
 CREATE TABLE IF NOT EXISTS public."user" (id bigint not null,	email varchar(255) NULL,	created_dt timestamptz NULL,    last_job_date timestamp without time zone default now()
 );
 
--- Drop table statement
 
 -- DROP TABLE public.repository;
 
